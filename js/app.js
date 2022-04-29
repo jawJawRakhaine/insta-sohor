842df02ab6f231b286bb18cc80588ef8739bc085
Jaw Jaw Rakhaine <jawjawrk246@gmail.com>
2022-04-29 12:31:49 +0600

    accordion solved

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ const switchTab = (id) => {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
-
+    document.getElementById("container-home").innerHTML = "";
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
